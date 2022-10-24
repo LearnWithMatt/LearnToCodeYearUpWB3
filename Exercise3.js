ddbb74c3dadd57329a8984612e63a4ceec8db937
mattchristenson <matt@rcc.team>
2022-10-24 12:02:23 -0500

    save point 1

---
+++ Exercise3.js
@@ -0,0 +1,20 @@
+
+
+function displayGreeting(name, timeOfDay){
+    let message = "Hello " + name + ", I hope you are having a great " + timeOfDay;
+    console.log(message);
+}
+
+
+let thename = "Safat";
+let thetimeofday = "afternoon"
+displayGreeting(thename, timeOfDay);
+
+
+//--------------
+
+function displayNumber(number){
+    console.log("the number is " + number)
+}
+
+displayNumber((5 + 7 + 9) / (4 - 2));
---
+++ Exercises2.js
@@ -0,0 +1,26 @@
+
+
+
+function displayAMessageTwice(theMessage){
+    console.log(theMessage);
+    console.log(theMessage);
+}
+
+let aMessageForEveryone = "This is the message";
+
+displayAMessageTwice(aMessageForEveryone);
+
+
+
+//------------------------
+
+
+function displayAMessageTwice(msg){
+    console.log(msg);
+    console.log(msg);
+}
+
+let msg = "This is the other message";
+
+displayAMessageTwice(msg);
+
